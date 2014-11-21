de7e4714aabe24055757afb5dea0119d8bd58369
cakemasher <r.wolthuis@st.hanze.nl>
2014-11-21 18:27:02 +0100

    Dienblad Class

--- Rick/Dienblad.java
+++ Rick/Dienblad.java
@@ -5,13 +5,11 @@
  * @version 18-11-2014
  */
  
-/* De ArrayList importeren. */
-// import java.util.ArrayList;
+/* Stack importeren zodat we deze kunnen gebruiken in onze class. */
 import java.util.Stack;
 
 public class Dienblad
 {
-	//private ArrayList<Artikel> artikelen;
 	private Stack<Artikel> artikelen;
 	
 	/* Constructor voor de class Dienblad */
@@ -33,35 +31,9 @@ public class Dienblad
 	}
 	
 	
-	/* Functie om het aantal artikelen op het dienblad te tellen */
-	public int getAantalArtikelen()
+	/* Functie waarbij de Stack artikelen word gereturned. */
+	public Stack<Artikel> getArtikelen ()
 	{
-		/* Het aantal elementen in de ArrayList tellen, en returnen. */
-		return this.artikelen.size();
-	}
-	
-	
-	/* Functie om totaal prijs van het dienblad te berekenen. */
-	public double getTotaalPrijs()
-	{
-		/* Tijdelijke double aanmaken, en deze de waarde 0 geven. */
-		double totaalPrijs	= 0;
-		
-			/* Elke item bij langs gaan in de ArrayList artikelen. Bij elke item word de prijs opgehaald van het object, en deze bij totaalPrijs opgeteld. */
-			//for (Artikel artikel : this.artikelen)
-				//totaalPrijs = totaalPrijs + artikel.getPrijs();
-				
-			/* Een while aanmaken waarbij alle items in de stack artikelen word bij langs gegaan. */
-			while(!this.artikelen.empty())
-			{
-				/* Tijdelijke variabele aanmaken en daarin het eerst volgende artikel in zetten uit de Stack. Dit item word daar ook meteen uit verwijderd. */
-				Artikel tijdelijkArtikel = this.artikelen.pop();
-				
-				/* De totaalprijs aanpassen en de prijs van het artikel bij het totaalprijs optellen. */
-				totaalPrijs = totaalPrijs + tijdelijkArtikel.getPrijs();
-			}
-		
-		/* De berekende totaal prijs terug geven. */
-		return totaalPrijs;
+		return this.artikelen;
 	}
 }
