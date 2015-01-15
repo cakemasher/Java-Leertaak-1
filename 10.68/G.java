6c10ee2f406852098a7095ad769969baecf7a98a
timzijl <timzijl@hotmail.com>
2015-01-15 11:47:30 +0100

    rick is men mattiee

---
+++ 10.68/G.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class G here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface G
+{
+    
+}
---
+++ 10.68/U.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class U here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class U extends X implements G
+{
+    
+}
